1ee829deaea6aef12212643488723a7f37a0ce63
Agustín <agustin.olmedo@greencodesoftware.com>
2020-12-17 11:54:29 -0300

    add database schema and data

---
+++ database/export_tables.sql
@@ -0,0 +1,3 @@
+COPY broadleaf_pull_requests TO 'broadleaf_pull_requests.csv' DELIMITER ',' CSV HEADER;
+COPY twitter4j_pull_requests TO 'twitter4j_pull_requests.csv' DELIMITER ',' CSV HEADER;
+COPY antlr4_pull_requests TO 'antlr4_pull_requests.csv' DELIMITER ',' CSV HEADER;
