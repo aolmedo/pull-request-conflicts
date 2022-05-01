ae42ee9bc24ab16f013295c04256087e05b819d1
Agustín Olmedo <agustin.olmedo@greencodesoftware.com>
2022-05-01 20:05:26 -0300

    ghtorrent data extraction. Print progress

--- ghtorrent_data_extraction/ghtorrent_extract_data.py
+++ ghtorrent_data_extraction/ghtorrent_extract_data.py
@@ -6,8 +6,10 @@ if __name__ == "__main__":
 
     ghtorrent_db = GHTorrentDB()
     projects = ghtorrent_db.get_selected_projects()
+    total_count = len(projects)
 
-    for project in projects:
+    for i, project in enumerate(projects):
+        print(i+1, "/", total_count)
         ghtorrent_db.extract_pull_requests_for_project(project)
         ghtorrent_db.export_pull_requests_for_project(project)
         ghtorrent_db.extract_commits_for_project(project)
