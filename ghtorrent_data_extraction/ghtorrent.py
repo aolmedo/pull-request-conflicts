5cf98982e6a2c40903167f88b30584322df5ed63
Agustín Olmedo <agustin.olmedo@greencodesoftware.com>
2022-05-01 19:01:20 -0300

    ghtorrent data extraction. Skip exception for duplicate tables.

--- ghtorrent_data_extraction/ghtorrent.py
+++ ghtorrent_data_extraction/ghtorrent.py
@@ -32,8 +32,8 @@ class GHTorrentDB(object):
 
     def get_selected_projects(self):
         projects = []
-        query = "SELECT * FROM selected_projects WHERE id = %(id)s"
-        ret = self.execute_query(query, {'id': 16922941})
+        query = "SELECT * FROM selected_projects ORDER BY created_at"
+        ret = self.execute_query(query)
         for data in ret:
             project = Project(*data)
             projects.append(project)
@@ -42,13 +42,19 @@ class GHTorrentDB(object):
     def extract_pull_requests_for_project(self, project):
         query = "CREATE TABLE {}_pull_requests AS (SELECT * FROM pull_requests WHERE base_repo_id = %(id)s)".\
                 format(project.name.lower())
-        self.execute(query, {'id': project.id})
+        try:
+            self.execute(query, {'id': project.id})
+        except psycopg2.errors.lookup("42P07"):
+            pass
 
     def extract_commits_for_project(self, project):
         query = "CREATE TABLE {}_commits AS (SELECT * FROM commits WHERE project_id = %(id)s "\
                 "OR id IN (SELECT head_commit_id FROM {}_pull_requests))".\
                 format(project.name.lower(), project.name.lower())
-        self.execute(query, {'id': project.id})
+        try:
+            self.execute(query, {'id': project.id})
+        except psycopg2.errors.lookup("42P07"):
+            pass
 
     def export_selected_projects(self):
         query = "COPY (SELECT * FROM selected_projects ORDER BY created_at) TO STDOUT " \
@@ -60,13 +66,13 @@ class GHTorrentDB(object):
     def export_pull_requests_for_project(self, project):
         query = "COPY (SELECT * FROM {}_pull_requests ORDER BY opened_at) TO STDOUT " \
                 "DELIMITER ',' CSV HEADER".format(project.name.lower())
-        file_path = settings.GHTORRENT_EXPORT_PATH + "/{}_pull_requests.csv".format(project.name.lower())
+        file_path = settings.GHTORRENT_EXPORT_PATH + "pull_requests/{}_pull_requests.csv".format(project.name.lower())
         with open(file_path, "w") as table_file:
             self.copy_to(query, table_file)
 
     def export_commits_for_project(self, project):
         query = "COPY (SELECT * FROM {}_commits ORDER BY created_at) TO STDOUT "\
                 "DELIMITER ',' CSV HEADER".format(project.name.lower())
-        file_path = settings.GHTORRENT_EXPORT_PATH + "/{}_commits.csv".format(project.name.lower())
+        file_path = settings.GHTORRENT_EXPORT_PATH + "commits/{}_commits.csv".format(project.name.lower())
         with open(file_path, "w") as table_file:
             self.copy_to(query, table_file)
