1162fe5c62c0a43e70d918b8f42be1cacb97dbe3
Agustín Olmedo <agustin.olmedo@greencodesoftware.com>
2022-07-02 10:33:32 -0300

    Take start date as the date of the first PR. Fix error when there is no PR in a time window.

--- ipe_analysis/management/commands/calculate_ipe_time_windows.py
+++ ipe_analysis/management/commands/calculate_ipe_time_windows.py
@@ -30,7 +30,8 @@ class Command(BaseCommand):
             generate_images = False
         for project in projects:
             time_windows = []
-            current_date = project.created_at.date()
+            first_pr = project.pull_requests.order_by('opened_at').first()
+            current_date = first_pr.opened_at.date()
             until_date = datetime.date(2019, 6, 1)  # GHTorrent last date
 
             while current_date < until_date:
@@ -42,43 +43,55 @@ class Command(BaseCommand):
                 date_to = time_window[1]
                 ipe_data = IPECalculation(project, date_from, date_to)
                 if not ipe_data.pull_requests:
-                    continue;
-                ipe_time_window = IPETimeWindow.objects.create(project=project, tw_size=time_interval,
-                                                               start_date=date_from, end_date=date_to,
-                                                               pull_requests_number=ipe_data.pull_requests.count(),
-                                                               pairwise_conflicts_number=ipe_data.pcga.
-                                                               pairwise_conflict_number,
-                                                               potential_conflict_resolutions_number=ipe_data.pcga.
-                                                               potential_conflicting_prs_number,
-                                                               unconflicting_pull_request_groups_number=len(
-                                                                   ipe_data.pcga.groups),
-                                                               historical_conflict_resolutions_number=ipe_data.
-                                                               historical_conflict_resolutions_number,
-                                                               historical_ipe=ipe_data.historical_ipe,
-                                                               optimized_conflict_resolutions_number=ipe_data.
-                                                               optimized_conflict_resolutions_number,
-                                                               optimized_ipe=ipe_data.optimized_ipe,
-                                                               ipe_improvement_percentage=ipe_data.
-                                                               ipe_improvement_percentage,
-                                                               cr_improvement_percentage=ipe_data.
-                                                               cr_improvement_percentage)
-                if generate_images:
-                    file_name = '{}_{}_{}.png'.format(project.name,
-                                                      date_from.strftime('%Y-%m-%d'),
-                                                      date_to.strftime('%Y-%m-%d'))
-                    blob = GraphDrawer().store_graph(ipe_data.pcga.convert_to_nx_graph(colored=False))
-                    ipe_time_window.pairwise_conflict_graph_image.save(file_name, ContentFile(blob.getvalue()),
-                                                                       save=True)
+                    IPETimeWindow.objects.create(project=project, tw_size=time_interval,
+                                                 start_date=date_from, end_date=date_to,
+                                                 pull_requests_number=0,
+                                                 pairwise_conflicts_number=0,
+                                                 potential_conflict_resolutions_number=0,
+                                                 unconflicting_pull_request_groups_number=0,
+                                                 historical_conflict_resolutions_number=0,
+                                                 historical_ipe=0,
+                                                 optimized_conflict_resolutions_number=0,
+                                                 optimized_ipe=0,
+                                                 ipe_improvement_percentage=0,
+                                                 cr_improvement_percentage=0)
+                else:
+                    ipe_time_window = IPETimeWindow.objects.create(project=project, tw_size=time_interval,
+                                                                   start_date=date_from, end_date=date_to,
+                                                                   pull_requests_number=ipe_data.pull_requests.count(),
+                                                                   pairwise_conflicts_number=ipe_data.pcga.
+                                                                   pairwise_conflict_number,
+                                                                   potential_conflict_resolutions_number=ipe_data.pcga.
+                                                                   potential_conflicting_prs_number,
+                                                                   unconflicting_pull_request_groups_number=len(
+                                                                       ipe_data.pcga.groups),
+                                                                   historical_conflict_resolutions_number=ipe_data.
+                                                                   historical_conflict_resolutions_number,
+                                                                   historical_ipe=ipe_data.historical_ipe,
+                                                                   optimized_conflict_resolutions_number=ipe_data.
+                                                                   optimized_conflict_resolutions_number,
+                                                                   optimized_ipe=ipe_data.optimized_ipe,
+                                                                   ipe_improvement_percentage=ipe_data.
+                                                                   ipe_improvement_percentage,
+                                                                   cr_improvement_percentage=ipe_data.
+                                                                   cr_improvement_percentage)
+                    if generate_images:
+                        file_name = '{}_{}_{}.png'.format(project.name,
+                                                          date_from.strftime('%Y-%m-%d'),
+                                                          date_to.strftime('%Y-%m-%d'))
+                        blob = GraphDrawer().store_graph(ipe_data.pcga.convert_to_nx_graph(colored=False))
+                        ipe_time_window.pairwise_conflict_graph_image.save(file_name, ContentFile(blob.getvalue()),
+                                                                           save=True)
 
-                    blob = GraphDrawer().store_graph(ipe_data.pcga.convert_to_nx_graph(colored=True))
-                    ipe_time_window.colored_pairwise_conflict_graph_image.save(file_name,
-                                                                               ContentFile(blob.getvalue()),
-                                                                               save=True)
+                        blob = GraphDrawer().store_graph(ipe_data.pcga.convert_to_nx_graph(colored=True))
+                        ipe_time_window.colored_pairwise_conflict_graph_image.save(file_name,
+                                                                                   ContentFile(blob.getvalue()),
+                                                                                   save=True)
 
-                    blob = GraphDrawer().store_graph(ipe_data.pcga.convert_pcgg_to_nx_graph(), height=1, width=1)
-                    ipe_time_window.pull_request_group_graph_image.save(file_name, ContentFile(blob.getvalue()),
-                                                                        save=True)
+                        blob = GraphDrawer().store_graph(ipe_data.pcga.convert_pcgg_to_nx_graph(), height=1, width=1)
+                        ipe_time_window.pull_request_group_graph_image.save(file_name, ContentFile(blob.getvalue()),
+                                                                            save=True)
 
-                    blob = ipe_data.plot()
-                    ipe_time_window.integration_trajectories_image.save(file_name, ContentFile(blob.getvalue()),
-                                                                        save=True)
+                        blob = ipe_data.plot()
+                        ipe_time_window.integration_trajectories_image.save(file_name, ContentFile(blob.getvalue()),
+                                                                            save=True)
