510eb0241a3a1620d1f8f5f0bbca46d368c8fab6
Agustín <agustin.olmedo@greencodesoftware.com>
2022-08-04 10:37:27 -0300

    upload ghtorrent data

---
+++ calculate_projects_ipe_stats.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+import os
+import sys
+import subprocess
+
+
+def main():
+    from pairwise_conflict_dataset.models import Project
+    projects = Project.objects.filter(id__gte=1, id__lte=283).order_by('created_at')
+    total_count = len(projects)
+    for i, project in enumerate(projects):
+        print(i + 1, "/", total_count)
+        print(project.name)
+        # calculate IPE time windows
+        print("Calculate Project IPE Stats")
+        subprocess.run(["python", "manage.py", "calculate_project_ipe_stats", "--project_name", project.name, "--tw_size", "14"],
+                       capture_output=True)
+
+
+if __name__ == "__main__":
+    # Init Django environment
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    parent_dir = os.path.dirname(current_dir)
+    sys.path.insert(0, parent_dir)
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pull_request_data_analysis.settings')
+
+    import django
+    django.setup()
+
+    main()
--- database/import_data.sql
+++
@@ -1,3 +0,0 @@
-COPY broadleaf_pull_requests FROM 'broadleaf_pull_requests.csv'  DELIMITER ',' CSV HEADER;
-COPY twitter4j_pull_requests FROM 'twitter4j_pull_requests.csv'  DELIMITER ',' CSV HEADER;
-COPY antlr4_pull_requests FROM 'antlr4_pull_requests.csv'  DELIMITER ',' CSV HEADER;
