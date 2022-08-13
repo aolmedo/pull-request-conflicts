2c3600969c26eb66d9e72899fc03855d0888d001
Agustín <agustin.olmedo@greencodesoftware.com>
2022-08-13 16:13:20 -0300

    update config.

--- pairwise_conflict_dataset/settings.py
+++ pairwise_conflict_dataset/settings.py
@@ -3,10 +3,10 @@ import os
 
 # Data Settings
 CURRENT_PATH = os.path.abspath(os.path.dirname(__file__))
-GHTORRENT_IMPORT_PATH = os.path.join(CURRENT_PATH, '../ghtorrent_data')
+GHTORRENT_IMPORT_PATH = os.path.join(CURRENT_PATH, '../ghtorrent_data/random_selection_projects')
 
 CONFLICT_MATRIX_PATH = os.path.join(CURRENT_PATH, '../conflict_matrix')
-REPOSITORIES_BASE_PATH = '/home/aolmedo/phd/projects'
+REPOSITORIES_BASE_PATH = 'projects'
 
 # Github API
-GITHUB_TOKEN = "9767f1c10603cf6a0ce5423bbe016b938b268451"
+GITHUB_TOKEN = ""
